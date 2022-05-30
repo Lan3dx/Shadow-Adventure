32ed003ae4a0e76b149767d8060298ca05c3c138
Nikita <nikitalavrukhinuk@gmail.com>
2022-05-31 00:18:48 +0300

    bug fix

--- Shadow Adventure/include/gravity.h
+++ Shadow Adventure/include/gravity.h
@@ -8,6 +8,6 @@
 #include "../include/spawn.h"
 #include "../include/map.h"
 
-void gravitationB(BMAP*, std::vector<std::vector<char>>&); // gravity for bullets
+void gravitationB(BMAP*, std::vector<std::vector<char>>&); // gravity for bullets 
 void gravitationP(PMAP*, std::vector<std::vector<char>>&); // gravity for players
 void gravitationM(MMAP*, std::vector<std::vector<char>>&); // gravity func for mob
