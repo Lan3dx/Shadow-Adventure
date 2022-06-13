5346c5486611e5dcb08edf2474d1de0d39420cc5
Nikita <nikitalavrukhinuk@gmail.com>
2022-06-13 15:42:07 +0300

    new config

--- Shadow Adventure/src/sys/config.cpp
+++ Shadow Adventure/src/sys/config.cpp
@@ -12,19 +12,19 @@ void clear()
 // basic console settings
 void std_config()
 {
-	void* activeBuffer = GetStdHandle(STD_OUTPUT_HANDLE);
-	SMALL_RECT zeroWindow = { 0, 0, 0, 0 };
-	COORD zBuffer = { 1, 1 };
-	SetConsoleWindowInfo(activeBuffer, TRUE, &zeroWindow);
-	SetConsoleScreenBufferSize(activeBuffer, zBuffer);
+	void* activeBuffer = GetStdHandle(STD_OUTPUT_HANDLE); 
+	SMALL_RECT zeroWindow = { 0, 0, 0, 0 }; 
+	COORD zBuffer = { 1, 1 }; 
+	SetConsoleWindowInfo(activeBuffer, TRUE, &zeroWindow); 
+	SetConsoleScreenBufferSize(activeBuffer, zBuffer); 
 
-	COORD bufferSize = { 63, 50 };
-	SMALL_RECT windowSize = { 0, 0, 63 - 1, 50 - 1 };
-	SetConsoleScreenBufferSize(activeBuffer, bufferSize);
-	SetConsoleWindowInfo(activeBuffer, TRUE, &windowSize);
+	COORD bufferSize = { 63, 50 }; 
+	SMALL_RECT windowSize = { 0, 0, 63 - 1, 50 - 1 }; 
+	SetConsoleScreenBufferSize(activeBuffer, bufferSize); 
+	SetConsoleWindowInfo(activeBuffer, TRUE, &windowSize); 
 
-	CONSOLE_CURSOR_INFO structCursorInfo; // Disabling the cursor
-	GetConsoleCursorInfo(activeBuffer, &structCursorInfo);
-	structCursorInfo.bVisible = FALSE;
-	SetConsoleCursorInfo(activeBuffer, &structCursorInfo);
+	CONSOLE_CURSOR_INFO structCursorInfo; // Disabling the cursor 
+	GetConsoleCursorInfo(activeBuffer, &structCursorInfo); 
+	structCursorInfo.bVisible = FALSE; 
+	SetConsoleCursorInfo(activeBuffer, &structCursorInfo); 
 }
