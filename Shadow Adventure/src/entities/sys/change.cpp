e454fcf2f4af4e89ddc081ae2fe1c3f5849b7481
Nikita <nikitalavrukhinuk@gmail.com>
2023-01-10 00:40:22 +0300

    bug fix

--- Shadow Adventure/src/entities/sys/change.cpp
+++ Shadow Adventure/src/entities/sys/change.cpp
@@ -21,11 +21,6 @@ void change(PMAP* players, std::string* selected)
 				*selected = ("player" + std::to_string(b + 1));
 				break;
 			}
-			else if (players->get().contains("player" + std::to_string(b - 1)) && *selected != ("player" + std::to_string(0)))
-			{
-				*selected = ("player" + std::to_string(b - 1));
-				break;
-			}
 			else
 			{
 				*selected = ("player" + std::to_string(0));
